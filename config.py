cbd1231f00ed58cc80f114e0a49ccc87777d10d1
abibimaryam <rriarij@gmail.com>
2025-07-09 16:49:23 +0500

    added search from internet

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-# from langchain_community.llms.ollama import Ollama
-from llama_index.llms.ollama import Ollama
+from langchain_community.llms.ollama import Ollama
+# from llama_index.llms.ollama import Ollama
 
 DATA_DIR = "/home/maryam/Документы/projects/bandmc/data"
 CHROMA_PATH = "chroma_db"
-med_model = Ollama(model="medllama2:latest", temperature=0)
+# model = Ollama(model="medllama2:latest", temperature=0)
+model=Ollama(model="gemma3:4b")
 # model=Ollama(model="tripolskypetr/gemma3-tools:4b", temperature=0,request_timeout=180.0)
 
