8de122b70f70983b82746792b914fe0adf6eb8a1
abibimaryam <rriarij@gmail.com>
2025-07-10 15:52:43 +0500

    work with gpt

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 from langchain_community.llms.ollama import Ollama
-# from llama_index.llms.ollama import Ollama
+
 
 DATA_DIR = "/home/maryam/Документы/projects/bandmc/data"
 CHROMA_PATH = "chroma_db"
-# model = Ollama(model="medllama2:latest", temperature=0)
-model=Ollama(model="alibayram/medgemma:latest")
-# model=Ollama(model="tripolskypetr/gemma3-tools:4b", temperature=0,request_timeout=180.0)
 
+# model=Ollama(model="alibayram/medgemma:latest",temperature=0)
+
+
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(
+    api_key="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",
+    model="gpt-4o-mini",  
+    temperature=0
+)
